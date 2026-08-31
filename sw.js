0190df4a86264c358410e3df963dc155ee88bf88
ana12gbrlllima-commits <ana12gbrlllima@gmail.com>
2026-08-31 20:14:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ponto-phenom-v8";
+const CACHE_NAME = "ponto-phenom-v9";
 const CORE_ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
